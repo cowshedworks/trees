8bfacc8730c170887e84775e3dd894e5293a6b17
Gavin Phillips <gavin@cowshedworks.co.uk>
2020-12-30 15:05:26 +0000

    Add PHPUnit config

---
+++ tests/FactoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CowshedWorks\Trees\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        
+    }
+}
