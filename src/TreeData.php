df40e8b20fc9a84d9ebfe7ebb0380cc1e872b147
Gavin Phillips <gavin@cowshedworks.co.uk>
2020-12-31 19:46:10 +0000

    Refactor TreeData class

--- src/TreeData.php
+++ src/TreeData.php
@@ -12,100 +12,43 @@ use CowshedWorks\Trees\UnitValues\Weight;
 
 class TreeData
 {
-    public static array $requiredvalidParameters = ['circumference', 'age', 'height', 'diameter'];
+    public static array $requiredvalidParameters = [
+        'circumference',
+        'age',
+        'height',
+        'diameter'
+    ];
+
     private array $speciesData;
+
     private $age;
+
     private $circumference;
+
     private $diameter;
+
     private $height;
+
     private $aboveGroundWeight;
+
     private $belowGroundWeight;
+
     private $totalGreenWeight;
+
     private $totalDryWeight;
+
     private $totalCarbonWeight;
+
     private $totalCarbonSequestered;
+    
     private $totalCarbonSequesteredPerYear;
 
     public function __construct(array $speciesData, array $treeData)
     {
         $this->speciesData = $speciesData;
-        $this->init($treeData);
-    }
-
-    private function init(array $treeData): void
-    {
         $this->buildProvidedAttributes($treeData);
-        $this->buildEmptyAttributes();
+        $this->resolveEmptyAttributes();
         $this->calculateWeights();
-    }
-
-    private function buildProvidedAttributes(array $treeData): void
-    {
-        foreach (self::$requiredvalidParameters as $parameter) {
-            if (false === isset($treeData[$parameter])) {
-                continue;
-            }
-
-            $values = [];
-
-            if (isset($parameter)) {
-                $values = preg_split('/(?<=[0-9])(?=[a-z]+)/i', $treeData[$parameter]);
-            }
-
-            $unitValueClass = '\CowshedWorks\Trees\UnitValues\\'.ucfirst($parameter);
-
-            if (count($values) === 2) {
-                $this->{$parameter} = new $unitValueClass($values[0], $values[1]);
-            }
-
-            if (count($values) === 1) {
-                $this->{$parameter} = new $unitValueClass($values[0], $unitValueClass::getDefault());
-            }
-        }
-    }
-
-    private function buildEmptyAttributes(): void
-    {
-        if ($this->age === null) {
-            if ($this->circumference) {
-                $this->age = new Age($this->circumference->getValue() / $this->getSpeciesData('attributes.growth-rate.annual-average-circumference.value'), 'years');
-            }
-
-            if ($this->height) {
-                $this->age = new Age($this->height->getValue() / $this->getSpeciesData('attributes.growth-rate.annual-average-height.value'), 'years');
-            }
-        }
-
-        if ($this->age === null) {
-            throw new Exception('Unable to resolve tree age');
-        }
-
-        if ($this->circumference === null) {
-            if ($this->diameter != null) {
-                $this->circumference = new Circumference($this->diameter->getValue() * M_PI, 'cm');
-            } else {
-                $this->circumference = new Circumference($this->age->getValue() * $this->getSpeciesData('attributes.growth-rate.annual-average-circumference.value'), 'cm');
-            }
-        }
-
-        if ($this->diameter === null) {
-            $this->diameter = new Diameter($this->circumference->getValue() / M_PI, 'cm');
-        }
-
-        if ($this->height === null) {
-            $this->height = new Height($this->age->getValue() * $this->getSpeciesData('attributes.growth-rate.annual-average-height.value'), 'cm');
-        }
-    }
-
-    private function calculateWeights(): void
-    {
-        $this->aboveGroundWeight = new Weight($this->getDiameterCoefficient() * pow($this->diameter->getValueIn('in'), 2) * $this->height->getValueIn('feet'), 'lbs');
-        $this->belowGroundWeight = new Weight($this->aboveGroundWeight->getValueIn('lbs') * 0.2, 'lbs');
-        $this->totalGreenWeight = new Weight($this->aboveGroundWeight->getValueIn('lbs') + $this->belowGroundWeight->getValueIn('lbs'), 'lbs');
-        $this->totalDryWeight = new Weight($this->totalGreenWeight->getValueIn('lbs') * 0.725, 'lbs');
-        $this->totalCarbonWeight = new Weight($this->totalDryWeight->getValueIn('lbs') * 0.5, 'lbs');
-        $this->totalCarbonSequestered = new Weight(3.6663 * $this->totalCarbonWeight->getValueIn('lbs'), 'lbs');
-        $this->totalCarbonSequesteredPerYear = new Weight($this->totalCarbonSequestered->getValue() / $this->age->getValue(), 'kg');
     }
 
     public function getDiameterCoefficient(): float
@@ -213,6 +156,73 @@ class TreeData
         return $this->totalCarbonSequesteredPerYear->getDescription();
     }
 
+    private function calculateWeights(): void
+    {
+        $this->calculateAboveGroundWeight();
+        $this->calculateBelowGroundWeight();
+        $this->calculateTotalGreenWeight();
+        $this->calculateTotalDryWeight();
+        $this->calculateTotalCarbonWeight();
+        $this->calculateTotalCarbonSequestered();
+        $this->calculateTotalCarbonSequesteredPerYear();
+    }
+
+    private function calculateTotalCarbonSequesteredPerYear(): void
+    {
+        $this->totalCarbonSequesteredPerYear = new Weight(
+            $this->totalCarbonSequestered->getValue() / $this->age->getValue(),
+            'kg'
+        );
+    }
+
+    private function calculateTotalCarbonSequestered(): void
+    {
+        $this->totalCarbonSequestered = new Weight(
+            3.6663 * $this->totalCarbonWeight->getValueIn('lbs'),
+            'lbs'
+        );
+    }
+
+    private function calculateTotalCarbonWeight(): void
+    {
+        $this->totalCarbonWeight = new Weight(
+            $this->totalDryWeight->getValueIn('lbs') * 0.5,
+            'lbs'
+        );
+    }
+
+    private function calculateTotalDryWeight(): void
+    {
+        $this->totalDryWeight = new Weight(
+            $this->totalGreenWeight->getValueIn('lbs') * 0.725,
+            'lbs'
+        );
+    }
+
+    private function calculateTotalGreenWeight(): void
+    {
+        $this->totalGreenWeight = new Weight(
+            $this->aboveGroundWeight->getValueIn('lbs') + $this->belowGroundWeight->getValueIn('lbs'),
+            'lbs'
+        );
+    }
+
+    private function calculateAboveGroundWeight(): void
+    {
+        $this->aboveGroundWeight = new Weight(
+            $this->getDiameterCoefficient() * pow($this->diameter->getValueIn('in'), 2) * $this->height->getValueIn('feet'),
+            'lbs'
+        );
+    }
+
+    private function calculateBelowGroundWeight(): void
+    {
+        $this->belowGroundWeight = new Weight(
+            $this->aboveGroundWeight->getValueIn('lbs') * 0.2,
+            'lbs'
+        );
+    }
+
     public static function validateTreeParameters(array $treeParameters): bool
     {
         $totalValidParams = count(self::$requiredvalidParameters);
@@ -220,5 +230,63 @@ class TreeData
         $validParamsNotProvided = array_diff(self::$requiredvalidParameters, array_keys($treeParameters));
 
         return count($validParamsNotProvided) != $totalValidParams;
+    }
+
+    private function buildProvidedAttributes(array $treeData): void
+    {
+        foreach (self::$requiredvalidParameters as $parameter) {
+            if (false === isset($treeData[$parameter])) {
+                continue;
+            }
+
+            $values = [];
+
+            if (isset($parameter)) {
+                $values = preg_split('/(?<=[0-9])(?=[a-z]+)/i', $treeData[$parameter]);
+            }
+
+            $unitValueClass = '\CowshedWorks\Trees\UnitValues\\'.ucfirst($parameter);
+
+            if (count($values) === 2) {
+                $this->{$parameter} = new $unitValueClass($values[0], $values[1]);
+            }
+
+            if (count($values) === 1) {
+                $this->{$parameter} = new $unitValueClass($values[0], $unitValueClass::getDefault());
+            }
+        }
+    }
+
+    private function resolveEmptyAttributes(): void
+    {
+        if ($this->age === null) {
+            if ($this->circumference) {
+                $this->age = new Age($this->circumference->getValue() / $this->getSpeciesData('attributes.growth-rate.annual-average-circumference.value'), 'years');
+            }
+
+            if ($this->height) {
+                $this->age = new Age($this->height->getValue() / $this->getSpeciesData('attributes.growth-rate.annual-average-height.value'), 'years');
+            }
+        }
+
+        if ($this->age === null) {
+            throw new Exception('Unable to resolve tree age');
+        }
+
+        if ($this->circumference === null) {
+            if ($this->diameter != null) {
+                $this->circumference = new Circumference($this->diameter->getValue() * M_PI, 'cm');
+            } else {
+                $this->circumference = new Circumference($this->age->getValue() * $this->getSpeciesData('attributes.growth-rate.annual-average-circumference.value'), 'cm');
+            }
+        }
+
+        if ($this->diameter === null) {
+            $this->diameter = new Diameter($this->circumference->getValue() / M_PI, 'cm');
+        }
+
+        if ($this->height === null) {
+            $this->height = new Height($this->age->getValue() * $this->getSpeciesData('attributes.growth-rate.annual-average-height.value'), 'cm');
+        }
     }
 }
