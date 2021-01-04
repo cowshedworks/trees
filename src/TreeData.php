3899f7c7b245c42cbc5f55b8eed714ee38bffffa
Cowshed Works Ltd <cowshedworks@users.noreply.github.com>
2021-01-04 15:04:02 +0000

    Apply fixes from StyleCI

--- src/TreeData.php
+++ src/TreeData.php
@@ -328,7 +328,7 @@ class TreeData
             if (count($values) === 1) {
                 $this->{$parameter} = $this->unitValueFactory->{$parameter}($values[0]);
             }
-            
+
             $this->logBuild("{$parameter} set from provided parameters");
         }
     }
@@ -374,6 +374,7 @@ class TreeData
     {
         if ($this->height && $this->heightAgeRegressionData != null) {
             (new AgeFromHeightRegression())->run($this);
+
             return;
         }
 
